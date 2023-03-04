4da75c5425999e48ea909293a2e54df4be457aec
GGbouttaBag <122798085+GGbouttaBag@users.noreply.github.com>
2023-03-04 02:14:05 -0800

    Update SFC_DISM_scan.bat

--- SFC_DISM_scan.bat
+++ SFC_DISM_scan.bat
@@ -1,4 +1,13 @@
 @echo off
+title PC Zipper By; GlockBaby
+color 0a
+echo PC Zipper By : GlockBaby https://discord.gg/QtRRhpZg
+echo This program will clean and improve preformance of you PC.
+pause
+cls
+echo Do you want to start this program?
+echo REMEMBER! IMPORTANT! Restart Computer After To See Full Preformance Fix!
+pause
 date /t & time /t
 echo Dism /Online /Cleanup-Image /StartComponentCleanup
 Dism /Online /Cleanup-Image /StartComponentCleanup
@@ -11,4 +20,11 @@ date /t & time /t
 echo SFC /scannow
 SFC /scannow
 date /t & time /t
-pause
+echo Done! Now restart Your PC Now or Later to apply the improvement. ^<press any key to continue>
+goto end
+
+:end
+pause 
+echo thank you for using my program - GlockBaby
+Timeout 2
+exit
